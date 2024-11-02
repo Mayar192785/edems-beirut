b2bb039f5305c8484add3fb68435b7d3ae4497ac
Mayar192785 <96396572+Mayar192785@users.noreply.github.com>
2024-11-02 06:37:50 +0200

    fix: mainpage responsive

--- src/Pages/MainPage/index.jsx
+++ src/Pages/MainPage/index.jsx
@@ -17,26 +17,26 @@ const MainPage = () => {
       style={{
         backgroundImage:
           'url("https://edams-restaurant.com/wp-content/uploads/2024/01/Edams-R.png")',
-        backgroundSize: "cover", // Cover the entire div
-        backgroundPosition: "center", // Center the image
+        backgroundSize: "cover",
+        backgroundPosition: "center",
       }}
     >
       <div className="absolute inset-0 bg-black opacity-50"></div>
-      <div className="relative z-10 flex flex-col justify-center items-center gap-4">
-        <div className="flex flex-col justify-center items-center gap-4 p-4">
-          <h1 className="text-[#E3BC7E] text-5xl">EDAM'S</h1>
-          <h2 className="text-white text-7xl font-bold">RESTAURANT</h2>
+      <div className="relative z-10 flex flex-col justify-center items-center gap-4 p-4">
+        <div className="flex flex-col justify-center items-center gap-4">
+          <h1 className="text-[#E3BC7E] text-4xl sm:text-5xl md:text-6xl lg:text-7xl">EDAM'S</h1>
+          <h2 className="text-white text-5xl sm:text-6xl md:text-7xl lg:text-8xl font-bold">RESTAURANT</h2>
         </div>
-        <div className="flex flex-col justify-center items-center gap-4 p-4 w-full">
+        <div className="flex flex-col justify-center items-center gap-4 w-full">
           <button
             onClick={toEnglish}
-            className="bg-[#efc337] px-9 py-5 rounded-md text-2xl"
+            className="bg-[#efc337] px-8 py-4 rounded-md text-xl sm:text-2xl w-full max-w-xs"
           >
             English Menu
           </button>
           <button
             onClick={toArabic}
-            className="bg-[#efc337] px-9 py-5  rounded-md text-2xl"
+            className="bg-[#efc337] px-8 py-4 rounded-md text-xl sm:text-2xl w-full max-w-xs"
           >
             القائمة العربية
           </button>
